71909f10472090f3fd078f2bfedaf54f0f5bf2b3
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-05-07 10:26:44 +0200

    muchos detalles de diseño y layout

--- frontend/js/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-fetch("../components/navbar.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("navbar-placeholder").innerHTML = data;
-  })
-
-});
