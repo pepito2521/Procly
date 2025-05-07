6e806943bf978c61d5a5c997a1a22fb2c730db63
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-05-07 10:46:31 +0200

    muchas cosas mas de diseño

--- frontend/js/mis_tickets.js
+++ frontend/js/mis_tickets.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetch("../components/navbar.html")
-      .then(res => res.text())
-      .then(data => {
-        document.getElementById("navbar-placeholder").innerHTML = data;
-      })
+  // INSERTAR NAVBAR EN LA PAGINA
+
+  fetch("../components/navbar.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("navbar-placeholder").innerHTML = data;
     
-    });
+  // RESALTAR NAVBAR-LINK ACTIVO
+      const currentPage = window.location.pathname.split("/").pop();
+      const links = document.querySelectorAll(".navbar-links a");
+    
+      links.forEach(link => {
+        if (link.getAttribute("href").includes(currentPage)) {
+          link.classList.add("active");
+          }
+        });
+      });
+  });
