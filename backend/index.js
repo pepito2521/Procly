36dee67c3befcd57f7860b6fc504dcefc0761c91
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-05-18 14:47:39 +0200

    configuer el abckend (index.js) para que sirva los archivos estaticos del frontend/public y tambien hice que redirija a index.html como pagina principal

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const app = require('./app');
+const path = require('path')
 
 const PORT = process.env.PORT || 3000;
 
+// SERVIR ARCHIVOS ESTATICOS DE FRONTEND/PUBLIC
+app.use(express.static(path.join(__dirname, '../frontend/public')));
+
+
+// REDIRIGIR LA RAÍZ AL INDEX.HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/public/index.html'));
+});
+
 // INICIAR EL SERVIDOR
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
