ec24a522ae85d8bfadfbb168dd5525f6438d2948
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-05-18 10:39:31 +0200

    instale la libreria de cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
-// index.js
 const app = require('./app');
 
-// Puerto desde .env o por defecto 3000
 const PORT = process.env.PORT || 3000;
 
-// Iniciar el servidor
+// INICIAR EL SERVIDOR
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
