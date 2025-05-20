991b0dd4fcf117f5958050d2359166780bc04333
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-05-20 12:46:10 +0200

    correcion en supabase.js

--- backend/config/supabase.js
+++ backend/config/supabase.js
@@ -8,4 +8,10 @@ const supabase = createClient(
   process.env.SUPABASE_ANON_KEY
 );
 
-module.exports = supabase;
+const supabaseService = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+module.exports = { supabase, supabaseService };
+
