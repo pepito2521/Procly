5b3ed82b7661dc8c175e8e4d84359e8b3da87c17
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-05-20 18:11:05 +0200

    correcion en login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', async (e) => {
       e.preventDefault();
   
-      const email = document.getElementById('username').value;
+      const email = document.getElementById('email').value;
       const password = document.getElementById('password').value;
   
       try {
