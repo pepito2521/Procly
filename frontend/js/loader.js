5cb1a0e5702a63a2c13e4dd3896b9e3975b5838c
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-07-07 14:14:33 +0200

    aa

--- frontend/js/loader.js
+++ frontend/js/loader.js
@@ -1,5 +1,3 @@
-import lottie from 'https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie.min.js';
-
 export async function cargarLoader() {
   document.body.classList.add("oculto");
 
@@ -8,8 +6,7 @@ export async function cargarLoader() {
     const html = await res.text();
     document.getElementById('loader-placeholder').innerHTML = html;
 
-    // Cargar animación Lottie
-    lottie.loadAnimation({
+    window.lottie.loadAnimation({
       container: document.getElementById('loader-animation'),
       renderer: 'svg',
       loop: true,
