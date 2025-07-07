2efc207625362ef245060d883f1634b8ad03af47
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-07-07 10:40:58 +0200

    ???

--- frontend/js/loader.js
+++ frontend/js/loader.js
@@ -1,21 +1,26 @@
 export async function cargarLoader() {
-    const res = await fetch('/components/loader.html');
-    const html = await res.text();
-    document.getElementById('loader-placeholder').innerHTML = html;
-
-  const esperarYOcultar = () => {
-    const loader = document.getElementById("loader");
-    if (loader) {
-      loader.classList.add("fade-out");
-      setTimeout(() => {
-        loader.style.display = "none";
-      }, 500);
+    try {
+      const res = await fetch('/components/loader.html');
+      const html = await res.text();
+      document.getElementById('loader-placeholder').innerHTML = html;
+    } catch (error) {
+      console.error('Error al cargar el loader:', error);
     }
-  };
-
-  if (document.readyState === "complete") {
-    esperarYOcultar();
-  } else {
-    window.addEventListener("load", esperarYOcultar);
+  
+    const esperarYOcultar = () => {
+      const loader = document.getElementById("loader");
+      if (loader) {
+        loader.classList.add("fade-out");
+        setTimeout(() => {
+          loader.style.display = "none";
+        }, 500);
+      }
+    };
+  
+    if (document.readyState === "complete") {
+      esperarYOcultar();
+    } else {
+      window.addEventListener("load", esperarYOcultar);
+    }
   }
-}
+  
