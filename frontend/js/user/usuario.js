ffcc3fe56b73039065662d702ac6acc959acd23d
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-07-14 21:19:29 +0200

    ert

--- frontend/js/user/usuario.js
+++ frontend/js/user/usuario.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
       archivo: "nuevo_ticket.html",
       js: "/js/user/nuevo_ticket.js",
       icon: `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus">
-        <path d="M12 5v14"/><path d="M5 12h14"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256">
+        <path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path>
       </svg>
       `
     },
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
       archivo: "mis_tickets.html",
       js: "/js/user/mis_tickets.js",
       icon: `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list">
-        <path d="M12 20H2"/><path d="M12 4H2"/><path d="M20 12H4"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256">
+        <path d="M32,64a8,8,0,0,1,8-8H216a8,8,0,0,1,0,16H40A8,8,0,0,1,32,64Zm8,72h72a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16Zm88,48H40a8,8,0,0,0,0,16h88a8,8,0,0,0,0-16Zm109.66,13.66a8,8,0,0,1-11.32,0L206,177.36A40,40,0,1,1,217.36,166l20.3,20.3A8,8,0,0,1,237.66,197.66ZM184,168a24,24,0,1,0-24-24A24,24,0,0,0,184,168Z"></path>
       </svg>
       `
     },
@@ -28,8 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
       archivo: "manual.html",
       js: "/js/user/manual.js",
       icon: `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book">
-        <path d="M12 6h4l3 9H5L8 6z"/><path d="M15 6v12"/><path d="M9 6v12"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256">
+        <path d="M164.44,105.34l-48-32A8,8,0,0,0,104,80v64a8,8,0,0,0,12.44,6.66l48-32a8,8,0,0,0,0-13.32ZM120,129.05V95l25.58,17ZM216,40H40A16,16,0,0,0,24,56V168a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,128H40V56H216V168Zm16,40a8,8,0,0,1-8,8H32a8,8,0,0,1,0-16H224A8,8,0,0,1,232,208Z"></path>
       </svg>
       `
     }
