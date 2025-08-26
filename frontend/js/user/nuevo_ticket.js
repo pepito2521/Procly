f6f6fb0070e57c1ffe66bdf7a80c091e3642f7a1
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-08-26 17:55:22 +0200

    APP#11

--- frontend/js/user/nuevo_ticket.js
+++ frontend/js/user/nuevo_ticket.js
@@ -566,13 +566,15 @@ export function initNuevoTicket() {
 
     // Update UI to show selected file
     const fileNameDisplay = fileUploadArea.querySelector('.file-name-display');
-    fileNameDisplay.style.display = 'flex';
-    fileNameDisplay.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#10B981" viewBox="0 0 256 256">
-        <path d="M149.61,85.71l-89.6,88a8,8,0,1,1,11.22,0L10.39,136a8,8,0,1,1,11.22-11.41L54.4,156.79l84-82.5a8,8,0,1,1,11.22,11.42Zm96.1-11.32a8,8,0,0,0-11.32-.1l-84,82.5-18.83-18.5a8,8,0,0,0-11.21,11.42l24.43,24a8,8,0,0,0,11.22,0l89.6-88A8,8,0,0,0,245.71,74.39Z"></path>
-      </svg>
-      ${file.name}
-    `;
+    if (fileNameDisplay) {
+      fileNameDisplay.style.display = 'flex';
+      fileNameDisplay.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#10B981" viewBox="0 0 256 256">
+          <path d="M149.61,85.71l-89.6,88a8,8,0,1,1,11.22,0L10.39,136a8,8,0,1,1,11.22-11.41L54.4,156.79l84-82.5a8,8,0,1,1,11.22,11.42Zm96.1-11.32a8,8,0,0,0-11.32-.1l-84,82.5-18.83-18.5a8,8,0,0,0-11.21,11.42l24.43,24a8,8,0,0,0,11.22,0l89.6-88A8,8,0,0,0,245.71,74.39Z"></path>
+        </svg>
+        ${file.name}
+      `;
+    }
 
     // Update the form data for submission
     window.selectedFile = file;
@@ -582,8 +584,10 @@ export function initNuevoTicket() {
   function clearFileSelection() {
     window.selectedFile = null;
     const fileNameDisplay = fileUploadArea.querySelector('.file-name-display');
-    fileNameDisplay.style.display = 'none';
-    fileNameDisplay.innerHTML = '';
+    if (fileNameDisplay) {
+      fileNameDisplay.style.display = 'none';
+      fileNameDisplay.innerHTML = '';
+    }
     adjuntoInput.value = '';
   }
 
